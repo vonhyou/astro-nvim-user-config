f6fea336f93a3ea605e9c778662a63e44d4389f4
vonhyou <moi@rfung.dev>
2023-10-05 12:47:42 -0400

    add community plugins

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "astrodark",
+  colorscheme = "kanagawa-dragon", -- "astrodark",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
--- plugins/community.lua
+++ plugins/community.lua
@@ -4,7 +4,11 @@ return {
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  {import = "astrocommunity.pack.cpp"},
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.pack.cpp" },
+  { import = "astrocommunity.media.vim-wakatime" },
+  { import = "astrocommunity.bars-and-lines.smartcolumn-nvim" },
+  { import = "astrocommunity.bars-and-lines.vim-illuminate" }, 
+  { import = "astrocommunity.scrolling.vim-smoothie" },
+  { import = "astrocommunity.colorscheme.kanagawa-nvim" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
