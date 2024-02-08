6eb5b5c78d6adcdd672bb92e4c4982f0133d9ebf
vonhyou <moi@rfung.dev>
2024-02-08 16:21:32 -0500

    add plugins for auto-save etc.

--- plugins/community.lua
+++ plugins/community.lua
@@ -5,14 +5,17 @@ return {
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
   -- lang packs
   { import = "astrocommunity.pack.cpp" },
-  { import = "astrocommunity.pack.lua"},
+  { import = "astrocommunity.pack.python-ruff" },
+  { import = "astrocommunity.pack.lua" },
 
   -- functionalities
   { import = "astrocommunity.media.vim-wakatime" },
   { import = "astrocommunity.bars-and-lines.smartcolumn-nvim" },
-  { import = "astrocommunity.bars-and-lines.vim-illuminate" }, 
+  { import = "astrocommunity.bars-and-lines.vim-illuminate" },
   { import = "astrocommunity.scrolling.vim-smoothie" },
+  { import = "astrocommunity.scrolling.mini-animate" },
   { import = "astrocommunity.colorscheme.kanagawa-nvim" },
-  { import = "astrocommunity.scrolling.mini-animate"},
+  { import = "astrocommunity.editing-support.auto-save-nvim" },
+  { import = "astrocommunity.editing-support.todo-comments-nvim" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
